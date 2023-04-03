4bafb88536761a52b0e8889b92c4198fc7f765be
fatima-imlaoui <imlaouifatimaezzahra@gmail.com>
2023-04-03 15:37:24 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+#if#ifndef MAIN_H
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
@@ -8,7 +8,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-void print_chessboard(char (*a)[8]);
 #endif
